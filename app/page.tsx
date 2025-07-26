c5162e71c6f02f2f595ed86f8677def0be6fe8df
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-07-26 22:54:55 +0100

    connecting to prisma orm

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { prisma } from "./utils/db";
 
-const page = () => {
+const getData = async () => {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imgUrl: true,
+      authorImg: true,
+      authorName: true,
+      id: true,
+      createdAt: true,
+    },
+  });
+  return data;
+};
+
+const page = async () => {
+  const data = await getData();
   return (
-    <div className="flex flex-col">
-      <h1>hello from the index page</h1>
+    <div className="py-6 flex flex-col">
+      <h1 className="text-3xl font-semibold tracking-tight">Latest Posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <h1 key={item.id}>{item.title}</h1>
+        ))}
+      </div>
     </div>
   );
 };
